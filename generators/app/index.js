a399a41799b3de099a24a43daa520b69505bf801
Michael Bowman <bowmanmc@gmail.com>
2015-07-02 20:05:30 -0400

    starting on the template layouts

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,2 +1,22 @@
 var generators = require('yeoman-generator');
-module.exports = generators.Base.extend();
+
+var AisGenerator = generators.Base.extend({
+
+    prompting: function() {
+        var done = this.async();
+
+        this.prompt({
+            type: 'input',
+            name: 'name',
+            message: 'Your project name',
+            default: this.appname
+        }, function(answers) {
+            this.log(answers.name);
+            done();
+        }.bind(this));
+
+    }
+
+});
+
+module.exports = AisGenerator;
