33eb7bf85cc652d026b0741124701dbd66e13cc6
Michael Bowman <bowmanmc@gmail.com>
2015-07-27 20:29:15 -0400

    make directories and bower, karma, package config files

--- generators/app/index.js
+++ generators/app/index.js
@@ -18,8 +18,29 @@ var AisGenerator = generators.Base.extend({
     },
 
     projectfiles: function() {
+        var layout = [
+            'app/fonts',
+            'app/images',
+            'app/modules/api',
+            'app/modules/components',
+            'app/modules/pages',
+            'app/modules/services',
+            'app/scripts',
+            'docs',
+        ];
+
+        var dir;
+        for (var i in layout) {
+            dir = layout[i];
+            console.log('Creating directory: ' + dir);
+            this.mkdir(dir);
+        }
+
         // Process files
+        this.template('_bower.json', 'bower.json');
         this.template('_gulpfile.js', 'gulpfile.js');
+        this.template('_karma.conf.js', 'karma.conf.js');
+        this.template('_package.json', 'package.json');
 
         // doesn't copy empty directories...
         this.directory('app');
